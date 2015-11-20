12f1724414cf7bcd51b200955213cfb432e035a8
Cedric <cedric.lor@gmail.com>
2015-11-20 01:46:50 +0100

    added editable zones for text and photo in the about page

---
+++ public/javascripts/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  console.log("hello");
+
+  // if they click on a boxed image, enlarge it
+  $('div.img-box a').click(function(event) {
+    event.preventDefault();
+    var box =
+      '<div id="lightbox">' +
+      '<p>Click to Close</p>' +
+      '<img src="' + $(this).attr('href') + '" alt="Enlarged" />' +
+      '</div>';
+    $('body').prepend(box);
+  });
+
+  // if they click an enlarged image, close it
+  $('body').on('click', '#lightbox',function(event) {
+    $('#lightbox').remove();
+  });
+
+});
